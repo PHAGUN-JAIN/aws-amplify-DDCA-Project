223a5c104ac2226ba829ad1556d512b013f09d0b
PHAGUN-JAIN <phagunjain@protonmail.com>
2022-02-18 12:20:45 +0530

    test improvement

--- src/routes/landingPage.js
+++ src/routes/landingPage.js
@@ -4,9 +4,9 @@ router.route("/").get((req, res) => {
   res.send("Hello World!");
 });
 
-router.route("/ejs").get((req, res) => {
-  res.render("index", { title: "DDCA Project" });
-  res.end();
+router.route("/ejs").get(async (req, res) => {
+  await res.render("index", { title: "DDCA Project" });
+  // res.end();
 });
 
 module.exports = router;
